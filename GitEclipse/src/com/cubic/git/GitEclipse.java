7354616adca54805e76c5f39b49fd58329b44248
ssushantG <sushant.shrestha@colorado.edu>
2017-01-21 16:27:06 -0500

    Intial Check in

---
+++ GitEclipse/src/com/cubic/git/GitEclipse.java
@@ -0,0 +1,12 @@
+package com.cubic.git;
+
+public class GitEclipse {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to GIT");
+		System.out.print("Just created a local repo");
+		
+	}
+
+}
