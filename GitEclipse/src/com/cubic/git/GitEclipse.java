1b416af73d337dd42814f32a7e8ca0487430e4b1
ssushantG <sushant.shrestha@colorado.edu>
2017-01-21 17:04:58 -0500

    Update GitEclipse.java

--- GitEclipse/src/com/cubic/git/GitEclipse.java
+++ GitEclipse/src/com/cubic/git/GitEclipse.java
@@ -8,6 +8,7 @@ public class GitEclipse {
 		System.out.println("Just created a local repo");
 		System.out.println("This is for third commit");
 		System.out.println("This is edid1 from the server);
+				   slkjfa;lksd
 	}
 
 }
