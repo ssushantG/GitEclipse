64c8bbee95149672adf32579d275d72e1b9f4c25
ssushantG <sushant.shrestha@colorado.edu>
2017-01-21 16:40:48 -0500

    2nd Commit

--- GitEclipse/src/com/cubic/git/GitEclipse.java
+++ GitEclipse/src/com/cubic/git/GitEclipse.java
@@ -3,7 +3,7 @@ package com.cubic.git;
 public class GitEclipse {
 
 	public static void main(String[] args) {
-
+		System.out.println("Checking out in server");
 		System.out.println("Welcome to GIT");
 		System.out.print("Just created a local repo");
 		
